d99758c9560328e34e9ecaeff49f00c5daa46343
Paweł Barański <kalghor@gmail.com>
2021-08-13 13:44:30 +0200

    Add entity class - City
    To represent data from a database

--- src/main/java/pl/kalghor/weather/model/City.java
+++ src/main/java/pl/kalghor/weather/model/City.java
@@ -1,2 +1,13 @@
-package pl.kalghor.weather.model;public class City {
+package pl.kalghor.weather.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String name;
+
 }
