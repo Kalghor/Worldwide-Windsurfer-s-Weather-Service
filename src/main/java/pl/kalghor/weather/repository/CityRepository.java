e1175fb1bcc17cb48fdf0c7947cd834c8f05ae0b
Paweł Barański <kalghor@gmail.com>
2021-08-13 13:46:31 +0200

    Add CityRepository
    For communication with the database

--- src/main/java/pl/kalghor/weather/repository/CityRepository.java
+++ src/main/java/pl/kalghor/weather/repository/CityRepository.java
@@ -1,2 +1,9 @@
-package pl.kalghor.weather.repository;public interface CityRepository {
+package pl.kalghor.weather.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.kalghor.weather.model.City;
+
+@Repository
+public interface CityRepository extends CrudRepository<City, Long> {
 }
