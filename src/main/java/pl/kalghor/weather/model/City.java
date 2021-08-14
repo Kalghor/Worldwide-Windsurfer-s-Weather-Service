a5d35a31a7e60a33a46acf6d7103cbc1db4262a2
Paweł Barański <kalghor@gmail.com>
2021-08-14 14:42:23 +0200

    Add fields lat i lon

--- src/main/java/pl/kalghor/weather/model/City.java
+++ src/main/java/pl/kalghor/weather/model/City.java
@@ -1,13 +1,30 @@
 package pl.kalghor.weather.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "city")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class City {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     public Long id;
+
+    @Column(name = "name")
     public String name;
+
+    @Column(name = "latitude")
+    public String latitude;
+
+    @Column(name = "longitude")
+    public String longitude;
 
 }
