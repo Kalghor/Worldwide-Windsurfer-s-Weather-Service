791077aca84b3c70b90c03ea6699fddb5723dd27
Paweł Barański <kalghor@gmail.com>
2021-08-14 14:41:49 +0200

    Expose endpoints in WeatherController

--- src/main/java/pl/kalghor/weather/controller/WeatherController.java
+++ src/main/java/pl/kalghor/weather/controller/WeatherController.java
@@ -1,7 +1,43 @@
 package pl.kalghor.weather.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pl.kalghor.weather.model.City;
+import pl.kalghor.weather.model.Weather;
+import pl.kalghor.weather.service.CityService;
+import pl.kalghor.weather.service.WeatherService;
 
-@Controller
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/app")
 public class WeatherController {
+
+    private WeatherService weatherService;
+    private CityService cityService;
+
+    public WeatherController(WeatherService weatherService, CityService cityService) {
+        this.weatherService = weatherService;
+        this.cityService = cityService;
+    }
+
+
+    @GetMapping("/today")
+    public Weather getCityWithBestWeatherConditionsToday() {
+        return weatherService.getBestWeatherToday(cityService.getCities(), LocalDate.now());
+    }
+
+    @GetMapping("/{date}")
+    public Weather getCityWithBestWeatherConditions(@PathVariable String date) {
+        LocalDate day = LocalDate.parse(date);
+        LocalDate formattedDate = LocalDate.parse(day.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        return weatherService.getBestWeatherToday(cityService.getCities(), formattedDate);
+    }
+
+    @GetMapping("/addCity/{cityName}/{latitude}/{longitude}")
+    public void addCityToDB(@PathVariable String cityName, @PathVariable String latitude, @PathVariable String longitude){
+        City city = new City(null, cityName, latitude, longitude);
+        cityService.addCity(city);
+    }
+
 }
