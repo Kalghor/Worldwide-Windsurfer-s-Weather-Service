2d9bedbb73f1ba806c2b683cd3e2319255cf230b
Paweł Barański <kalghor@gmail.com>
2021-08-14 12:03:58 +0200

    Add weatherClient
    To separate communications with external api from business logic

--- src/main/java/pl/kalghor/weather/web/client/WeatherClient.java
+++ src/main/java/pl/kalghor/weather/web/client/WeatherClient.java
@@ -1,8 +1,13 @@
-package pl.kalghor.weather.client;
+package pl.kalghor.weather.web.client;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import pl.kalghor.weather.model.dto.WeatherDto;
+import pl.kalghor.weather.model.City;
+import pl.kalghor.weather.web.dto.WeatherDto;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class WeatherClient {
@@ -12,10 +17,12 @@ public class WeatherClient {
     private final String API_KEY = "b5ee118d173b478ba7f582d2c4ce98fc";
 
 
-    public WeatherDto getWeather() {
-        WeatherDto forObject = restTemplate.getForObject(URL + "daily?city=Raleigh&key={API_KEY}", WeatherDto.class, API_KEY);
-        System.out.println(forObject);
-        return forObject;
-//        return restTemplate.getForObject(URL + "daily?city=Raleigh&key={API_KEY}", Object.class, API_KEY);
+    public Set<WeatherDto> getWeatherForCities(Set<City> cities) {
+        Set<WeatherDto> forecasts = new HashSet<>();
+        for(City c : cities){
+            WeatherDto dto = restTemplate.getForObject(URL + "daily?city={city}}&key={API_KEY}", WeatherDto.class, c.name, API_KEY);
+            forecasts.add(dto);
+        }
+        return forecasts;
     }
 }
